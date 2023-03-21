c5abd5c044db62a15f9aa42c87b90e7a26d8feb8
mnyu123 <whdudwo5582@gmail.com>
2023-03-21 09:53:31 +0900

    0321-다시풀이

---
+++ SoloProject/src/Exam2.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int hiddenNum = r.nextInt(100); // 숨겨진 숫자
+
+        Scanner sc = new Scanner(System.in);
+        int num1 = 0;
+        do {
+            System.out.print("숫자를 입력하세요: ");
+            num1 = sc.nextInt(); // 내가 입력하는 숫자
+
+            if (num1 > hiddenNum) {
+                System.out.println("입력한 숫자가 더 큽니다.");
+
+            } 
+            else if (num1 < hiddenNum) {
+                System.out.println("숨겨진 숫자가 더 큽니다.");
+            }
+        } while (num1 != hiddenNum);
+        {
+            System.out.println("찾았습니다.");
+        }
+
+    }
+}
