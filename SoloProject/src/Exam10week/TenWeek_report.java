2520864e414356e3d468498b08f5ac4a09b49478
mnyu123 <whdudwo5582@gmail.com>
2023-05-04 17:05:01 +0900

    레포트 수정

--- SoloProject/src/Exam10week/TenWeek_report.java
+++ SoloProject/src/Exam10week/TenWeek_report.java
@@ -3,53 +3,50 @@ package Exam10week;
 import java.io.*;
 
 public class TenWeek_report {
+    public static void main(String[] args) {
+        String InFilePath = "C:\\Users\\whdud\\Desktop\\고급자바프로그래밍\\테스트\\Test1.txt";
+        String OutFilePath = "C:\\Users\\whdud\\Desktop\\고급자바프로그래밍\\테스트\\Test1-copy.txt";
 
-	public static void main(String[] args) {
-		IsFileCopy(null, null);
-		System.out.println("파일 복사 시작");
-	}
+        if (IsFileCopy(InFilePath, OutFilePath)) {
+            System.out.println("파일 복사 성공");
+        } else {
+            System.out.println("파일 복사 실패");
+        }
+    }
 
-	public static boolean IsFileCopy(String InFilePath, String OutFilePath) {
-		// TODO Auto-generated method stub
-		InFilePath = "test7.txt";
-		OutFilePath = "test7-복사본.txt";
-		String str = "복사할 내용";
-		try {
-			File f = new File(InFilePath);
-			if (!f.exists()) {
-				f.createNewFile();
-			}
-			FileInputStream fis = new FileInputStream(f);
+    public static boolean IsFileCopy(String InFilePath, String OutFilePath) {
+        File InFile = new File(InFilePath);
+        File OutFile = new File(OutFilePath);
 
-			int i = 0;
+        String str = " ";
 
-			while ((i = fis.read()) != -1) {
-				System.out.print((char) i);
-			}
-			fis.close();
-			System.out.println("파일 작성");
-		} catch (Exception e) {
-			e.getMessage();
-		}
+        try {
+            FileInputStream fis = new FileInputStream(InFile);
+            FileOutputStream fos = new FileOutputStream(OutFile);
 
-		try {
-			File f = new File(OutFilePath);
-			if (!f.exists()) {
-				f.createNewFile();
-			}
-			FileOutputStream fos = new FileOutputStream(f);
+            if (!InFile.exists()) {
+                InFile.createNewFile();
+            }
 
-			byte[] b = str.getBytes();
-			fos.write(b);
-			fos.close();
-			System.out.println("파일 복사 성공");
+            if (!OutFile.exists()) {
+                OutFile.createNewFile();
+            }
 
-		} catch (Exception e) {
-			e.getMessage();
-		}
-		return false;
-	}
+            byte[] b = str.getBytes();
+            int length;
+            while ((length = fis.read(b)) > 0) {
+                fos.write(b, 0, length);
+            }
 
-	
+            fis.close();
+            fos.close();
 
+            return true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
 }
